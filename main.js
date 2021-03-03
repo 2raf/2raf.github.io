c87861dce42cebca6332925eba6e515a8ec30b16
Kat Graham <kmgraham811@gmail.com>
2021-03-02 21:20:06 -0500

    slideshow part 3! Trying something different...

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 window.onload = function(){
     var slides = document.querySelectorAll('#slides .slides');
     var currentSlide = 0;
-    var slideInterval = setInterval(nextSlide,2000);
+    var slideInterval = setInterval(nextSlide,5000);
 
     function nextSlide(){
       slides[currentSlide].className = 'slides';
