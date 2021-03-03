903ead9af5f30d01606d8669009824f51a95407c
Kat Graham <kmgraham811@gmail.com>
2021-03-02 20:00:48 -0500

    slideshow part 1!

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+//Created by katdotexe on 3/2/2021
+
+
+//carousel function to flip through slides automatically
+var myIndex = 0;
+carousel();
+
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) { //loop of slides
     x[i].style.display = "none";  
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
+  myIndex++; //increase my index
+  if (myIndex > x.length) {myIndex = 1}  //reset index to 1
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 5000); // Change image every 5 seconds
 }
