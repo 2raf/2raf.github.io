6159aefff76d6ace435c4e893124c2a1dfe008ec
Kat Graham <kmgraham811@gmail.com>
2021-03-02 20:47:04 -0500

    slideshow part 3! Trying something different...

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //Created by katdotexe on 3/2/2021
 
-var slides = document.querySelectorAll('#slides .slide');
+var slides = document.querySelectorAll('#slides .slides');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide(){
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'slides';
   currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'slides showing';
 }
